41c482c99a050c74f72ccb825225302233ecf11c
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-03-22 12:30:35 -0300

    refactor(dto): melhora organização de Usuario e Plano

---
+++ server/Academy/src/main/java/com/example/Academy/dto/PlanoDTO.java
@@ -0,0 +1,13 @@
+package com.example.Academy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlanoDTO {
+     private Long id;
+     private String nome;
+     private String descricao;
+     private Double preco;
+}
