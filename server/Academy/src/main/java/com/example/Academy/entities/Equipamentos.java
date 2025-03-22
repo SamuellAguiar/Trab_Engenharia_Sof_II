c9f23fa996c42068eedd136d2d2cc0ae04050132
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-03-22 12:25:13 -0300

    feat(model): adiciona entidade Equipamentos

---
+++ server/Academy/src/main/java/com/example/Academy/entities/Equipamentos.java
@@ -0,0 +1,33 @@
+package com.example.Academy.entities;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "equipamentos")
+public class Equipamentos {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+     private String nome;
+     private String preco;
+     private String disponibilidade; 
+
+     @OneToMany(mappedBy = "equipamento", cascade = CascadeType.ALL, orphanRemoval = true)
+     private List<Aluguel> alugueis;
+}
